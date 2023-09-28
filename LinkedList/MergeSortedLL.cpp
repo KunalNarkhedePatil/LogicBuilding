3b2bf3d8815f44fe1b7fe174429281fb44e3fef0
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2023-09-28 19:58:01 +0530

    commit

--- LinkedList/MergeSortedLL.cpp
+++ LinkedList/MergeSortedLL.cpp
@@ -1,5 +1,36 @@
 #include"linkedlist.h"
+class Demo
+{
+    public:
+    SinglyLinearLL *sobj1,*sobj2;
 
+    Demo(SinglyLinearLL *sobj1,SinglyLinearLL *sobj2)
+    {
+        this->sobj1=sobj1;
+        this->sobj2=sobj2;
+    }
+    void mergeSortedLL() 
+    {
+        PNODE temp=sobj1->first;
+
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=sobj2->first;
+    }
+    void display()
+    {
+        PNODE temp=sobj1->first;
+
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
 int main()
 {
     SinglyLinearLL *sobj1=new SinglyLinearLL();
@@ -16,5 +47,11 @@ int main()
     
     sobj1->display();
     sobj2->display();
+
+    Demo *dobj=new Demo(sobj1,sobj2);
+
+    dobj->mergeSortedLL();
+    cout<<"After two sorted linked list:"<<endl;
+    dobj->display();
     return 0;
 }
--- LinkedList/Practice.cpp
+++ LinkedList/Practice.cpp
@@ -1,154 +1,248 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Node
 {
-    public:
-
+public:
     int data;
     Node *next;
 
     Node(int iVal)
     {
-        this->data=iVal;
-        this->next=NULL;
+        this->data = iVal;
+        this->next = NULL;
     }
 };
-typedef class Node * PNODE;
+typedef class Node *PNODE;
 class Linkedlist
 {
-    private:
-
+private:
     PNODE first;
     int iSize;
+
 public:
     Linkedlist()
     {
-        first=NULL;
-        iSize=0;
+        first = NULL;
+        iSize = 0;
     }
 
     void insertAtLast(int iVal)
     {
-        PNODE newn=new Node(iVal);
-        if(first==NULL)
+        PNODE newn = new Node(iVal);
+        if (first == NULL)
         {
-            first=newn;
+            first = newn;
         }
         else
         {
-            PNODE temp=first;
+            PNODE temp = first;
 
-            while(temp->next!=NULL)
+            while (temp->next != NULL)
             {
-                temp=temp->next;
+                temp = temp->next;
             }
-            temp->next=newn;
+            temp->next = newn;
         }
         iSize++;
     }
     void Display()
     {
-        PNODE temp=first;
-        
-        while(temp!=NULL)
+        PNODE temp = first;
+
+        while (temp != NULL)
         {
-            cout<<temp->data<<"->";
-            temp=temp->next;
+            cout << temp->data << "->";
+            temp = temp->next;
         }
-        cout<<"NULL\n";
+        cout << "NULL\n";
     }
-    bool checkArrPalindromic(int Arr[],int iSize)
+    bool checkArrPalindromic(int Arr[], int iSize)
     {
-         int iStart=0;
-         int iEnd=iSize-1;
-         bool iFlag=true;
-         while(iStart<=iEnd)
-         {
-            if(Arr[iStart]!=Arr[iEnd])
+        int iStart = 0;
+        int iEnd = iSize - 1;
+        bool iFlag = true;
+        while (iStart <= iEnd)
+        {
+            if (Arr[iStart] != Arr[iEnd])
             {
-                iFlag=false;
+                iFlag = false;
                 break;
             }
             iStart++;
             iEnd--;
-         }
-         if(iFlag==true)
-         {
+        }
+        if (iFlag == true)
+        {
             return true;
-         }
-         else
-         {
+        }
+        else
+        {
             return false;
-         }
+        }
     }
     bool checkPalindromic()
     {
-       int Arr[iSize];
+        int Arr[iSize];
 
-       PNODE temp=first;
-         int i=0;
-       while(temp!=NULL)
-       {
-           Arr[i]=temp->data;
-           temp=temp->next;
-           i++;
-       }
-       if(checkArrPalindromic(Arr,iSize))
-       {
-            cout<<"Palinderomic LL"<<endl;
-       }
-       else
-       {
-         cout<<"Not Palinderomic LL"<<endl;
-       }
+        PNODE temp = first;
+        int i = 0;
+        while (temp != NULL)
+        {
+            Arr[i] = temp->data;
+            temp = temp->next;
+            i++;
+        }
+        if (checkArrPalindromic(Arr, iSize))
+        {
+            cout << "Palinderomic LL" << endl;
+        }
+        else
+        {
+            cout << "Not Palinderomic LL" << endl;
+        }
     }
     PNODE reverseLL(PNODE first)
     {
-        PNODE prevNode=NULL;
-        PNODE currNode=first;
-        PNODE nextNode=NULL;
+        PNODE prevNode = NULL;
+        PNODE currNode = first;
+        PNODE nextNode = NULL;
 
-        while(currNode!=NULL)
+        while (currNode != NULL)
         {
-            nextNode=currNode->next;
-            currNode->next=prevNode;
+            nextNode = currNode->next;
+            currNode->next = prevNode;
 
-            //update 
-            prevNode=currNode;
-            currNode=nextNode;
+            // update
+            prevNode = currNode;
+            currNode = nextNode;
         }
         return prevNode;
     }
     void checkLLPalindromic()
     {
-        PNODE temp=NULL;
-        temp=first;
-        int iPos=(iSize/2)+1;
-        for(int i=1;i<iPos;i++)
+        PNODE temp = NULL;
+        temp = first;
+        int iPos = (iSize / 2) + 1;
+        for (int i = 1; i < iPos; i++)
         {
-            temp=temp->next;
+            temp = temp->next;
         }
-        PNODE middle=temp;
-        PNODE RevLL=reverseLL(middle);
-        temp=first;
-        int iFlag=1;
-        while(temp!=middle->next)
+        PNODE middle = temp;
+        PNODE RevLL = reverseLL(middle);
+        temp = first;
+        int iFlag = 1;
+        while (temp != middle->next)
         {
-            if(RevLL->data!=temp->data)
+            if (RevLL->data != temp->data)
             {
-                iFlag=0;
+                iFlag = 0;
                 break;
             }
-            temp=temp->next;
-            RevLL=RevLL->next;
+            temp = temp->next;
+            RevLL = RevLL->next;
         }
-        if(iFlag==1)
+        if (iFlag == 1)
         {
-            cout<<"Palindromic LL"<<endl;
+            cout << "Palindromic LL" << endl;
         }
         else
         {
-            cout<<"not Palindromic LL"<<endl;
+            cout << "not Palindromic LL" << endl;
+        }
+    }
+    void sortZeroOneTwo()
+    {
+        PNODE ZeroFirst=NULL;
+        PNODE ZeroLast=NULL;
+
+        PNODE OneFirst=NULL;
+        PNODE OneLast=NULL;
+
+        PNODE TwoFirst=NULL;
+        PNODE TwoLast=NULL;
+
+        PNODE currNode=first;
+
+        while(currNode!=NULL)
+        {
+            if(currNode->data==0)
+            {
+                if(ZeroFirst==NULL && ZeroLast==NULL)
+                {
+                    ZeroFirst=currNode;
+                    ZeroLast=currNode;
+                }
+                else
+                {
+                    ZeroLast->next=currNode;
+                    ZeroLast=currNode;
+                }
+            }
+            else if(currNode->data==1)
+            {
+                if(OneFirst==NULL && OneLast==NULL)
+                {
+                    OneFirst=currNode;
+                    OneLast=currNode;
+                }
+                else
+                {
+                    OneLast->next=currNode;
+                    OneLast=currNode;
+                }
+            }
+            else if(currNode->data==2)
+            {
+                if(TwoFirst==NULL && TwoLast==NULL)
+                {
+                    TwoFirst=currNode;
+                    TwoLast=currNode;
+                }
+                else
+                {
+                    TwoLast->next=currNode;
+                    TwoLast=currNode;
+                }
+            }
+            currNode=currNode->next;
+        }
+        first=ZeroFirst;
+        ZeroLast->next=OneFirst;
+        OneLast->next=TwoFirst;
+        TwoLast->next=NULL;
+        PNODE temp=first;
+
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    void checkCircular()
+    {
+        PNODE temp = first->next;
+
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=first;
+
+        temp=first;
+
+        while (temp != NULL && temp != first)
+        {
+            temp = temp->next;
+        }
+        cout<<first<<"And "<<temp<<endl;
+        if (temp == NULL)
+        {
+            cout << "Not Circular LL" << endl;
+        }
+        else
+        {
+            cout << "Circular LL" << endl;
         }
     }
     int count()
@@ -160,18 +254,31 @@ int main()
 {
     Linkedlist obj;
 
-    obj.insertAtLast(30);
-    obj.insertAtLast(20);
-    obj.insertAtLast(10);
-    obj.insertAtLast(20);
-    obj.insertAtLast(30);
+    // obj.insertAtLast(30);
+    // obj.insertAtLast(20);
+    // obj.insertAtLast(10);
+    // obj.insertAtLast(20);
+    // obj.insertAtLast(30);
+
+    obj.insertAtLast(0);
+    obj.insertAtLast(1);
+    obj.insertAtLast(2);
+    obj.insertAtLast(1);
+    obj.insertAtLast(0);
+    obj.insertAtLast(2);
+    obj.insertAtLast(1);
+    obj.insertAtLast(2);
+    obj.insertAtLast(0);
 
     obj.Display();
 
-    //obj.checkPalindromic();
+    // obj.checkPalindromic();
 
-    obj.checkLLPalindromic();
+    // obj.checkLLPalindromic();
 
-    cout<<"Size is:"<<obj.count()<<endl;
+    //obj.checkCircular();
+    obj.sortZeroOneTwo();
+
+    cout << "Size is:" << obj.count() << endl;
     return 0;
 }
