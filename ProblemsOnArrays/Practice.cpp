01d320e634fc9276a93cc4416f1660a6520b3b55
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-01-29 19:06:59 +0530

    commit

--- ProblemsOnArrays/Practice.cpp
+++ ProblemsOnArrays/Practice.cpp
@@ -1,67 +1,69 @@
-#include<iostream>
+#include <iostream>
+#include<stdlib.h>
 using namespace std;
-void Display(int *Arr,int size)
+void Display(int *Arr, int size)
 {
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<Arr[i]<<" ";
+        cout << Arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-void printAllNegative(int *Arr,int size)
+void printAllNegative(int *Arr, int size)
 {
-   for(int i=0;i<size;i++)
-   {
-      if(Arr[i]<0)
-      {
-          cout<<Arr[i]<<" ";
-      }
-   }
-   cout<<endl;
-}
-int sumOfAllElement(int *Arr,int size)
-{
-    int Sum=0;
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        Sum=Sum+Arr[i];
+        if (Arr[i] < 0)
+        {
+            cout << Arr[i] << " ";
+        }
+    }
+    cout << endl;
+}
+int sumOfAllElement(int *Arr, int size)
+{
+    int Sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        Sum = Sum + Arr[i];
     }
     return Sum;
 }
-int MaxInArray(int *Arr,int size)
+int MaxInArray(int *Arr, int size)
 {
-    int Max=Arr[0];
+    int Max = Arr[0];
 
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        if(Arr[i]>Max)
+        if (Arr[i] > Max)
         {
-            Max=Arr[i];
+            Max = Arr[i];
         }
     }
     return Max;
 }
-void SecondMaxinArray(int *Arr,int size)
+void SecondMaxinArray(int *Arr, int size)
 {
-    int max=MaxInArray(Arr,size);
-    cout<<max<<endl;
-    int secMax=0;
-    for(int i=0;i<size;i++)
+    int max = MaxInArray(Arr, size);
+    cout << max << endl;
+    int secMax = 0;
+    for (int i = 0; i < size; i++)
     {
-        if(Arr[i]>secMax && Arr[i]<max)
+        if (Arr[i] > secMax && Arr[i] < max)
         {
-            secMax=Arr[i];
+            secMax = Arr[i];
         }
     }
-    cout<<"Second Maximum number is "<<secMax<<endl;
+    cout << "Second Maximum number is " << secMax << endl;
 }
-void countEvenOdd(int *Arr,int size)
+void countEvenOdd(int *Arr, int size)
 {
-    int CntEven=0;
-    int CntOdd=0;
+    int CntEven = 0;
+    int CntOdd = 0;
 
-    for(int i=0;i<size;i++){
-        if(Arr[i]%2==0)
+    for (int i = 0; i < size; i++)
+    {
+        if (Arr[i] % 2 == 0)
         {
             CntEven++;
         }
@@ -70,38 +72,57 @@ void countEvenOdd(int *Arr,int size)
             CntOdd++;
         }
     }
-    cout<<"Number of Even Element "<<CntEven<<endl;
-    cout<<"Number of Odd Element "<<CntOdd<<endl;
+    cout << "Number of Even Element " << CntEven << endl;
+    cout << "Number of Odd Element " << CntOdd << endl;
 }
-void copyOneToSec(int *Arr,int *Brr,int size)
+void copyOneToSec(int *Arr, int *Brr, int size)
 {
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        Brr[i]=Arr[i];
+        Brr[i] = Arr[i];
     }
+}
+void insertNewElement(int *Arr, int *size, int No, int pos)
+{
+    int n = *size;
+    Arr = (int *)realloc(Arr, sizeof(int) * (n + 1));
+    cout<<sizeof(Arr)<<endl;
+    for (int i = n; i >= 0; i--)
+    {
+        if (i == pos)
+        {
+            Arr[i] = No;
+            break;
+        }
+        Arr[i] = Arr[i - 1];
+    }
+    *size = *size + 1;
 }
 int main()
 {
-    int size=0;
-    cout<<"Enter the number of element in the array"<<endl;
-    cin>>size;
+    int size = 0;
+    cout << "Enter the number of element in the array" << endl;
+    cin >> size;
 
-    cout<<"Enter the element in the array"<<endl;
+    cout << "Enter the element in the array" << endl;
 
-    int *Arr=new int[size];
-    int *Brr=new int[size];
+    int *Arr = new int[size];
+    int *Brr = new int[size];
 
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        cin>>Arr[i];
+        cin >> Arr[i];
     }
-    Display(Arr,size);
-    //printAllNegative(Arr,size);
-    //cout<<"Sum of all element in the array"<<sumOfAllElement(Arr,size)<<endl;
-    //MaxInArray(Arr,size);
-    //SecondMaxinArray(Arr,size);
-    //countEvenOdd(Arr,size);
-    //copyOneToSec(Arr,Brr,size);
-    //Display(Brr,size);
+    Display(Arr, size);
+    // printAllNegative(Arr,size);
+    // cout<<"Sum of all element in the array"<<sumOfAllElement(Arr,size)<<endl;
+    // MaxInArray(Arr,size);
+    // SecondMaxinArray(Arr,size);
+    // countEvenOdd(Arr,size);
+    // copyOneToSec(Arr,Brr,size);
+    // Display(Brr,size);
+    insertNewElement(Arr, &size, 5, 3);
+    cout<<size<<endl;
+    Display(Arr, size);
     return 0;
 }
