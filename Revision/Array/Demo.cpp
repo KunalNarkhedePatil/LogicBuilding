69d343661ab0b3e80df4a348ac39860affea64ef
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-03-05 19:22:18 +0530

    commit

---
+++ Revision/Array/Demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int iNo=0;
+    cout<<"Enter the number "<<endl;
+    cin>>iNo;
+    cout<<"Number is"<<iNo<<endl;
+    return 0;
+}
