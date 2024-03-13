d04b8be9b2ff21e244411cc861d1db8eafe1d0af
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-03-13 19:50:04 +0530

    commmit

--- CollectionFramework/ListDemo.java
+++ CollectionFramework/ListDemo.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 public class ListDemo {
     public static void main(String[] args) {
-        List<String> obj1=new ListDemo();
+        List<String> obj1=new ArrayList<>();
+
+        obj1.add("Kunal");
+        obj1.add("Pavan");
+        obj1.add("Mayur");
+
+        Iterator iobj=obj1.iterator();
+
+        while(iobj.hasNext())
+        {
+            System.out.println(iobj.next());
+        }
+
+        for(String obj:obj1)
+        {
+            System.out.println(obj);
+        }
+    
 
 
     }
